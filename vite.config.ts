1e155c87454bde928f8a2fcacf63d2411e0cf383
jansorianowork <jansoriano.work@gmail.com>
2025-05-04 15:23:00 +0800

    Update for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
+   base: '/vs-zero-kit/',
   plugins: [
     react(),
     ...(process.env.NODE_ENV !== "production" &&
